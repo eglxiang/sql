0ca417bd6cb0a1a3b1cd4aabed68547a297443f6
Xiang Xiang <cnwhu@yahoo.com>
2017-01-16 23:58:49 -0500

    Add files via upload

--- detect_barcode.py
+++ detect_barcode.py
@@ -1,35 +1,43 @@
-import numpy as np
+# import pyimagesearch import packages
+import simple_barcode_detection
 import argparse
 import cv2
 
+# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i","--image",required=True,help="path to the image file")
-args=vars(ap.parse_args())
+ap.add_argument("-v", "--video", help="path to the video file")
+args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# if the video path was not supplied, grab the reference to the camera
+if not args.get("video", False):
+	camera = cv2.VideoCapture(0)
+# otherwise, load the video
+else:
+	camera = cv2.VideoCapture(args["video"])
 
-gradX = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=1, dy=0, ksize=-1)
-gradY = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=0, dy=1, ksize=-1)
+# keep looping over the frames
+while True:
+	# grab the current frame
+	(grabbed, frame) = camera.read()
 
-gradient = cv2.subtract(gradX, gradY)
-gradient = cv2.convertScaleAbs(gradient)
+	# check to see if we have reached the end of the video
+	if not grabbed:
+		break
 
-blurred = cv2.blur(gradient, (9,9))
-(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+	# detect the barcode in the image
+	box = simple_barcode_detection.detect(frame)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21,7))
-closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+	# if a barcode was found, draw a bounding box on the frame
+	cv2.drawContours(frame, [box], -1, (0,255,0), 2)
 
-closed = cv2.erode(closed, None, iterations = 4)
-closed = cv2.dilate(closed,None, iterations = 4)
+	# show the frame and record if the user presses a key
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
 
-(cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
 
-rect = cv2.minAreaRect(c)
-box = np.int0(cv2.cv.BoxPoints(rect))
-
-cv2.drawContours(image, [box], -1, (0,255,0), 3)
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
